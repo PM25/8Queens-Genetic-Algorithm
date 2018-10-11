a894c13af990ca14f237a674bd261f1bce5373e3
Einz <jason199786109@gmail.com>
2018-10-11 12:43:58 +0800

    Add comments to make code more clear

--- genetic_alogorithm.py
+++ genetic_alogorithm.py
@@ -2,10 +2,17 @@ import random
 import heapq
 import matplotlib.pyplot as plt
 
-queen_count = 8
-try_times = 1000000
+# Feel free to mass with argument below
+queen_count = 8 # Board size and Queen amount
+try_times = 1000000 # Maximum generation of child
+child_amount = 4 # Only work with even number
+initial_population = 12 # Starting population size
+mutate_times = 1
+static_analysis = True # Static Analysis will cause slow performance
 
 
+# Randomly generate a population size of {count}
+# with each sequence size of {len}
 def initial(len, count):
     seq = []
     for seq_step in range(count):
@@ -17,6 +24,7 @@ def initial(len, count):
     return seq
 
 
+# Give a sequence and return with a fitness score (higher mean better)
 def fitness(seq):
     nonattack_pair = 0
 
@@ -36,6 +44,7 @@ def fitness(seq):
     return (nonattack_pair / 2)
 
 
+# Select {count} number of sequence with probability base on their score
 def selection(seq_scores, count):
     score_accumulate = []
     sum = 0
@@ -55,6 +64,7 @@ def selection(seq_scores, count):
     return result
 
 
+# Give a list of sequences, slice and combine them randomly
 def crossover(seqs):
     childs = []
 
@@ -66,6 +76,8 @@ def crossover(seqs):
     return childs
 
 
+# Give a list of sequences and randomly change number
+# More {times} mean mutate more
 def mutation(seqs, times):
     for seq in seqs:
         for step in range(times):
@@ -75,6 +87,8 @@ def mutation(seqs, times):
     return seqs
 
 
+# Initial with {population} amount of number
+# and choose the top {top_count} individuals in population
 def initial_best(top_count, population):
     seq_list = initial(queen_count, population)
     result = []
@@ -93,33 +107,53 @@ def initial_best(top_count, population):
 
 
 
+# Start from here
 if(__name__ == "__main__"):
+    # Calculate the terminate condition (fitness score)
     ans_score = (queen_count * (queen_count - 1)) / 2
-    plt.title(str(queen_count) + "-Queens Genetic Alogorithm\n"
-                + "Answer score = " + str(ans_score))
-    plt.ylabel("Fitness score")
-    plt.xlabel("Iter")
+    # Show information to user
+    if(static_analysis):
+        plt.title(str(queen_count) + "-Queens Genetic Alogorithm\n"
+                    + "Answer score = " + str(ans_score))
+        plt.ylabel("Fitness score")
+        plt.xlabel("Iter")
+    else:
+        print(str(queen_count) + "-Queens Genetic Algorithm")
+        print("Answer score = " + str(ans_score))
+        print('-'*10 + '\n')
 
-    top_individuals = initial_best(4, 12)
-    plt.scatter([1, 2, 3, 4], [individual[0] for individual in top_individuals])
+    # Initial a population and choose best individual in it
+    top_individuals = initial_best(child_amount, initial_population)
+    # Starting individuals average score
+    score_avg = sum([individual[0] for individual in top_individuals]) / child_amount
+    if(static_analysis):
+        plt.scatter(0, score_avg)
+        plt.pause(1)
 
-    for step in range(4, try_times):
-        selected_parent = selection(top_individuals, 4)
+    # Genetic Algorithm with maximum {try_times} of try
+    for step in range(1, try_times):
+        selected_parent = selection(top_individuals, child_amount)
         children = crossover(selected_parent)
-        result_children = mutation(children, 1)
+        result_children = mutation(children, mutate_times)
 
+        # Replace {top_individuals} with {result_children}
         top_individuals.clear()
         score_avg = 0
         for child in result_children:
             score = fitness(child)
+            # Calculate this generation's average score
             score_avg = score_avg + score/len(result_children)
-
+            # Terminate condition
             if(score == ans_score):
-                print(child)
-                plt.scatter(step, score)
-                plt.show()
+                print("After", step, "of Generations,")
+                print("The algorithm found one of the solution!")
+                print("Sequence:", child)
+                if(static_analysis):
+                    plt.scatter(step, score)
+                    plt.show()
                 exit()
-
+            # Replace {top_individuals} with {result_children}
             top_individuals.append((score, child))
-        plt.scatter(step, score_avg)
-        plt.pause(0.01)
+        if(static_analysis):
+            plt.scatter(step, score_avg)
+            plt.pause(0.01)
